5d84fc99c4bf6ed92ff29c87f189140c9d808616
Seras <adrian_etto@yahoo.com>
2022-02-08 13:42:43 +0200

    climb ladders

--- Assets/Scripts/HelperScripts/TagManager.cs
+++ Assets/Scripts/HelperScripts/TagManager.cs
@@ -51,6 +51,7 @@ public class Tags
 {
     public const string GROUND_TAG = "Ground";
     public const string PLAYER_TAG = "Player";
+    public const string LADDER_TAG = "Ladder";
 
     public const string RIGHT_HAND_TAG = "RightHand";
     public const string RIGHT_TOE_TAG = "RightToe";
---
+++ Assets/Scripts/PlayerScripts/LadderController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderController : MonoBehaviour
+{
+    public float speedUpDown = 3.2f;
+    private PlayerControlsScript _playerControlsScript;
+    private PlayerMovement _playerMovement;
+    private Rigidbody _playerBody;
+    public bool isClimbing;
+
+    void Start()
+    {
+        _playerControlsScript = GetComponent<PlayerControlsScript>();
+        _playerMovement = GetComponent<PlayerMovement>();
+        _playerBody = GetComponent<Rigidbody>();
+        isClimbing = false;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        print("ENTER: " + collider.gameObject.tag);
+        if (collider.gameObject.CompareTag(Tags.LADDER_TAG))
+        {
+            GetComponent<PlayerMovement>().enabled = false;
+            isClimbing = true;
+        }
+    }
+    
+    void OnTriggerExit(Collider collider)
+    {
+        print("EXIT: " + collider.gameObject.tag);
+        if (collider.gameObject.CompareTag(Tags.LADDER_TAG))
+        {
+            GetComponent<PlayerMovement>().enabled = true;
+            isClimbing = false;
+        }
+    }
+ 
+    
+    void FixedUpdate()
+    {
+        DetectMovement();
+    }
+
+    void DetectMovement()
+    {
+        if (isClimbing)
+        {
+            _playerBody.velocity = new Vector3(
+                _playerControlsScript.controls.HorizontalAxis() * (-_playerMovement.walkSpeed), 
+                _playerControlsScript.controls.VerticalAxis() * (speedUpDown),
+                _playerBody.velocity.z);
+        }
+    }
+    
+}

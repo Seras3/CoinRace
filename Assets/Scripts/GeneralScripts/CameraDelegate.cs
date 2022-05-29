4d2688859f55eaabfaa2f90a12480feb46eeaff8
Seras <adrian_etto@yahoo.com>
2022-05-29 17:20:21 +0300

    sound source warnings fix

--- Assets/Scripts/GeneralScripts/CameraDelegate.cs
+++ Assets/Scripts/GeneralScripts/CameraDelegate.cs
@@ -14,20 +14,12 @@ public class CameraDelegate : MonoBehaviour
     
     private Vector2 xyPlayer1, xyPlayer2;
 
-    private AudioListener mainCameraAudioListener, player1CameraAudioListener;
-
-
 
     private void Start()
     {
-        mainCameraAudioListener = mainCamera.GetComponent<AudioListener>();
-        player1CameraAudioListener = player1Camera.GetComponent<AudioListener>();
-
         // Disable all here for solving stacking cameras problem
-        mainCameraAudioListener.enabled = false;
         mainCamera.enabled = false;
         
-        player1CameraAudioListener.enabled = false;
         player1Camera.enabled = false;
         player2Camera.enabled = false;
         mapCamera.enabled = false;
@@ -40,23 +32,19 @@ public class CameraDelegate : MonoBehaviour
         
         if (Vector2.Distance(xyPlayer1, xyPlayer2) > collideOffset)
         {
-            mainCameraAudioListener.enabled = false;
             mainCamera.enabled = false;
             
             player1Camera.enabled = true;
-            player1CameraAudioListener.enabled = true;
             player2Camera.enabled = true;
             mapCamera.enabled = true;
         }
         else
         {
-            player1CameraAudioListener.enabled = false;
             player1Camera.enabled = false;
             player2Camera.enabled = false;
             mapCamera.enabled = false;
 
             mainCamera.enabled = true;
-            mainCameraAudioListener.enabled = true;
         }
     }
     
